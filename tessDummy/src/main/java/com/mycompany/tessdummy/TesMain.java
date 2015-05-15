4bec7d5b236a3501d2f1ebef13b727443580efba
Kasun Balasooriya <kmbkck1@gmail.com>
2015-05-15 14:08:31 +0530

    Added the project and files

---
+++ tessDummy/src/main/java/com/mycompany/tessdummy/TesMain.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.tessdummy;
+
+import java.io.File;
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
+
+/**
+ *
+ * @author kasun
+ */
+public class TesMain {
+    
+    public static void main(String[] args) {
+        System.setProperty("jna.library.path", "32".equals(System.getProperty("sun.arch.data.model")) ? "lib/win32-x86" : "lib/win32-x86-64");
+
+        File imageFile = new File("input/eurotext.tif");
+        Tesseract instance = Tesseract.getInstance();  // JNA Interface Mapping
+        // Tesseract1 instance = new Tesseract1(); // JNA Direct Mapping
+        // File tessDataFolder = LoadLibs.extractTessResources("tessdata"); // Maven build bundles English data
+        // instance.setDatapath(tessDataFolder.getAbsolutePath());
+
+        try {
+            String result = instance.doOCR(imageFile);
+            System.out.println(result);
+        } catch (TesseractException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+    
+}
