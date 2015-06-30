7abddd8b20f03cffde629927feb4d38346bcbd15
Kasun Balasooriya <kmbkck1@gmail.com>
2015-06-30 19:56:31 +0530

    added the method get file list to read from a file list and do ocr for all the files

--- src/main/java/com/mycompany/tessdummy/TesMain.java
+++ src/main/java/com/mycompany/tessdummy/TesMain.java
@@ -11,6 +11,9 @@ import java.io.File;
 import javax.naming.NamingException;
 import net.sourceforge.tess4j.TesseractException;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import net.sourceforge.tess4j.Tesseract;
 import net.sourceforge.tess4j.Tesseract1;
 import net.sourceforge.tess4j.util.LoadLibs;
 
@@ -106,8 +109,8 @@ public class TesMain {
 //                  String sql;
 //                  sql = "INSERT INTO output_text (file_location,output_string) values('"+filelocation+"','"+output+"')";
                   Statement s=(Statement) conn.createStatement();
-                  s.executeUpdate("INSERT INTO output_text (project_id,file_location,output_string) values('"+project_id+"','"+filelocation+"','"+output+"')");
-                  
+                 // s.executeUpdate("INSERT INTO output_text (project_id,file_location,output_string) values('"+project_id+"','"+filelocation+"','"+output+"')");
+                  s.executeUpdate("INSERT INTO output_text (file_location,output_string) values('"+filelocation+"','"+output+"')");
                   s.close();
                   conn.close();
                   System.out.println("One Record added!!");
@@ -115,28 +118,58 @@ public class TesMain {
                  
             
         }
-
+        
+        
+        static File[] getFilelist(String dir){
+           File folder=new File(dir);
+           File [] listOfFiles=folder.listFiles();
+           
+           //print the list of files and folders
+           
+           for(int i=0; i<listOfFiles.length ;i++){
+               if(listOfFiles[i].isFile() && (listOfFiles[i].getName().toLowerCase().endsWith(".png") || listOfFiles[i].getName().toLowerCase().endsWith(".jpg") || listOfFiles[i].getName().toLowerCase().endsWith(".tif") || listOfFiles[i].getName().toLowerCase().endsWith(".bmp")) || listOfFiles[i].getName().toLowerCase().endsWith(".pdf") ){
+                   System.out.println("File :"+ listOfFiles[i]);
+               }else if (listOfFiles[i].isDirectory()){
+                 //  System.out.println("Folder :"+listOfFiles[i]);
+               }
+                      
+              
+           }
+           
+             return listOfFiles;
+        }
                 public static void main(String[] args) throws NamingException, SQLException {
-                    
+                   String result,filename;
                     System.setProperty("jna.library.path", "32".equals(System.getProperty("sun.arch.data.model")) ? "lib/win32-x86" : "lib/win32-x86-64");
                     
-                    File imageFile = new File("eurotext.tif");
-                    //Tesseract instance = Tesseract.getInstance();  // JNA Interface Mapping
-                     Tesseract1 instance = new Tesseract1(); // JNA Direct Mapping
+                    File[] imageFile = getFilelist("/home/kasun/Dummy_Tes/input_images/");
+                    Tesseract instance = Tesseract.getInstance();  // JNA Interface Mapping
+                    instance.setLanguage("eng");
+                     //Tesseract1 instance = new Tesseract1(); // JNA Direct Mapping
                     File tessDataFolder = LoadLibs.extractTessResources("tessdata"); // Maven build bundles English data
                     instance.setDatapath(tessDataFolder.getAbsolutePath());
 
                     try {
-                        String result = instance.doOCR(imageFile);
-                        System.out.println(result);
-                        Connection con=TesMain.connctToDb();
-                        TesMain.insertOutput(con,1,"eurotext.tif", result);
+//                        String result = instance.doOCR(imageFile);
+//                        System.out.println(result);
+//                        Connection con=TesMain.connctToDb();
+//                        TesMain.insertOutput(con,1,"eurotext.tif", result);
+                        
+                        for(int i=0;i<imageFile.length;i++){
+                            filename=imageFile[i].getName();//get the file name
+                            result=instance.doOCR(imageFile[i]);
+                            System.out.println(result + "\n");
+                            Connection con = TesMain.connctToDb();
+                            TesMain.insertOutput(con, i, imageFile[i].getPath(), result);
+                            
+                        }
+                        
                         
                     } catch (TesseractException e) {
                         System.err.println(e.getMessage());
                     }
 
-
+                 
 
     
         
